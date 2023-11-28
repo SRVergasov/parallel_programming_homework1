b468c9c2e075ce01ca41715a70b52e116bd220e5
Shamil Vergasov <90715539+SRVergasov@users.noreply.github.com>
2023-11-29 00:25:17 +0300

    Update task2_1.cpp

--- mpi/task2_1/task2_1.cpp
+++ mpi/task2_1/task2_1.cpp
@@ -1,76 +1,108 @@
 #include <iostream>
 #include <mpi.h>
-#include <random>
 
-int main(int argc, char** argv)
-{
-    int rank;
+int rand(int min, int max) {
+    return min + std::rand() % max;
+}
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, 100);
+int* generateArray(int length) {
+    int* arr = new int[length];
+    for (int i = 0; i < length; ++i) {
+        arr[i] = rand(0, 10);
+    }
+    return arr;
+}
+
+void printArray(int* arr, int length) {
+
+    for (int i = 0; i < length; ++i) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+const int a = 5;
+const int b = 6;
+const int N = 10;
+
+int main(int argc, char** argv) {
+    int rank, size;
 
     MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Status status;
-    int count;
 
-    const int SIZE = 100;
-    const int PART = 20;
+    int elementsPerProcess = N / (size - 1);
+    int elementsForLastProcess = elementsPerProcess;
 
-    const int A = 2;
-    const int B = 3;
+    if (N % (size - 1) > 0) {
+        elementsPerProcess++;
+        elementsForLastProcess = N % elementsPerProcess;
+    }
 
     if (rank == 0) {
-        int x[SIZE];
-        int y[SIZE];
-        int z[SIZE];
+        int *x = generateArray(N);
+        int *y = generateArray(N);
+        int *z = new int[N];
 
-        for (int i = 0; i < SIZE; ++i) {
-            x[i] = dist(gen);
-            y[i] = dist(gen);
-        }
-
+        printf("a: %d\n", a);
+        printf("b: %d\n", b);
         printf("x: ");
-        for (int i = 0; i < SIZE; i++) {
-            printf("%d ", x[i]);
-        }
-        printf("\ny: ");
-        for (int i = 0; i < SIZE; i++) {
-            printf("%d ", y[i]);
-        }
-        printf("\n");
+        printArray(x, N);
+        printf("y: ");
+        printArray(y, N);
 
-        int n = 1;
-        for (int i = 0; i < SIZE; i += PART, n++) {
-            MPI_Send(&x[i], PART, MPI_INT, n, 10, MPI_COMM_WORLD);
-            MPI_Send(&y[i], PART, MPI_INT, n, 10, MPI_COMM_WORLD);
+
+        int startIndex = 0;
+        for (int i = 0; i < size; i++) {
+            if (i == 0)
+                continue;
+            int sizeToTransfer = elementsPerProcess;
+            if (i == size - 1)
+                sizeToTransfer = elementsForLastProcess;
+
+            MPI_Send(&x[startIndex], sizeToTransfer, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&y[startIndex], sizeToTransfer, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+            startIndex += elementsPerProcess;
         }
 
-        n = 1;
-        for (int i = 0; i < SIZE; i += PART, n++) {
-            MPI_Recv(&z[i], PART, MPI_INT, n, 10, MPI_COMM_WORLD, &status);
+
+        startIndex = 0;
+        for (int i = 0; i < size; i++) {
+            if (i == 0)
+                continue;
+
+            int sizeToTransfer = elementsPerProcess;
+            if (i == size - 1)
+                sizeToTransfer = elementsForLastProcess;
+
+            MPI_Recv(&z[startIndex], sizeToTransfer, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            startIndex += elementsPerProcess;
         }
 
         printf("z: ");
-        for (int i = 0; i < SIZE; i++) {
-            printf("%d ", z[i]);
-        }
-    }
-    else {
-        MPI_Probe(0, 10, MPI_COMM_WORLD, &status);
+        printArray(z, N);
+    } else {
+        int count;
+        MPI_Status status;
+
+        MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
         MPI_Get_count(&status, MPI_INT, &count);
-        int x[PART];
-        int y[PART];
-        int z[PART];
 
-        MPI_Recv(&x[0], count, MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
-        MPI_Recv(&y[0], count, MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
+        int *x = new int[count];
+        int *y = new int[count];
+        int *z = new int[count];
 
-        for (int i = 0; i < count; ++i) {
-            z[i] = A * x[i] + B * y[i];
+        MPI_Recv(x, count, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(y, count, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+        for (int i = 0; i < count; i++) {
+            z[i] = a * x[i] + b * y[i];
         }
-        MPI_Send(&z, count, MPI_INT, 0, 10, MPI_COMM_WORLD);
+
+        MPI_Send(z, count, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
